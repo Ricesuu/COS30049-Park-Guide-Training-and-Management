ff543c99e427d757aca115baaffba6610cae9b7a
MiinGXx <144193816+MiinGXx@users.noreply.github.com>
2025-04-11 23:58:21 +0800

    Developed basic navigation tab + working navigation system (WIP)

---
+++ frontend/mobile/my-app/app/_layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Tabs } from "expo-router";
+
+const _layout = () => {
+    return <Tabs />;
+};
+
+export default _layout;
