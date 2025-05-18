ff7d8bb9ebf0079bf4c67497ed2b380a3d557655
Ruiziq <mohdhaziqsolamee@gmail.com>
2025-05-18 13:26:09 +0800

    CLEAR

--- frontend/web/parkguidenext/constants/constants.js
+++
@@ -1,2 +0,0 @@
-// Constants for the web frontend
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://192.168.0.175:3000";
--- frontend/web/visitor/app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ContactPage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-3xl font-bold">Contact</h1>
-    </main>
-  );
-}
